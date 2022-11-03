83f7045e15cd08a201a779528df156542f7dde48
Pan Liu <us.panliu@gmail.com>
2022-11-03 00:45:41 -0700

    create and place custom map markers

---
+++ src/data/markersData.js
@@ -0,0 +1,67 @@
+const markersData = [
+  {
+    "id": 0,
+    "title": "entrance",
+    "lat": 33.1260830339591,
+    "lng": -117.31149650823423,
+    "url": "../public/entrance.png"
+  },
+  {
+    "id": 1,
+    "title": "dino_island",
+    "lat": 33.12649010178619,
+    "lng": -117.31262788986075,
+    "url": "../public/dino.png"
+  },
+  {
+    "id": 2,
+    "title": "movie_world",
+    "lat": 33.12680896378903,
+    "lng": -117.31355141164045,
+    "url": "../public/movie_world.png"
+  },
+  {
+    "id": 3,
+    "title": "imagination_zone",
+    "lat": 33.127297830624144,
+    "lng": -117.31048711361252,
+    "url": "../public/imagination_zone.png"
+  },
+  {
+    "id": 4,
+    "title": "ninjago_world",
+    "lat": 33.12692218643314,
+    "lng": -117.31089364906347,
+    "url": "../public/ninjago_world.png"
+  },
+  {
+    "id": 5,
+    "title": "miniland_USA",
+    "lat": 33.12775762820445,
+    "lng": -117.31154061420955,
+    "url": "../public/miniland_USA.png"
+  },
+  {
+    "id": 6,
+    "title": "deep_sea_adventure",
+    "lat": 33.129015471778544,
+    "lng": -117.31143307116457,
+    "url": "../public/deep_sea_adventure.png"
+  },
+  {
+    "id": 7,
+    "title": "DUPLO_playtown",
+    "lat": 33.127342636818064,
+    "lng": -117.31244049534183,
+    "url": "../public/DUPLO_playtown.png"
+  },
+  {
+    "id": 8,
+    "title": "fun_town",
+    "lat": 33.127867131690905,
+    "lng": -117.31319993751636,
+    "url": "../public/fun_town.png"
+  },
+]
+
+export default markersData;
